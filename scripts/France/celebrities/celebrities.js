5db4f723db0a2dcd46a553db4e32c6b77406ca50
GersonGmd <gersondiketama@yahoo.com>
2021-08-24 10:57:16 -0500

    first page commit

---
+++ scripts/France/celebrities/celebrities.js
@@ -0,0 +1,35 @@
+
+
+const franceCelebrities = 
+[
+
+
+    {
+        name:"Maitre Gims",
+        image:"image.png",
+        links: "links to celebrites"
+    },
+
+
+    {
+        name:"Maitre Gims",
+        image:"image.png",
+        links: "links to celebrites"
+    },
+
+
+
+    {
+        name:"Maitre Gims",
+        image:"image.png",
+        links: "links to celebrites"
+    },
+
+
+
+    {
+        name:"Maitre Gims",
+        image:"image.png",
+        links: "links to celebrites"
+    }
+]
---
+++ scripts/France/cities/cities.js
@@ -0,0 +1,39 @@
+
+
+const citiesData = 
+[
+    {
+        cityName:"Paris",
+        image: "image.png",
+        info: "Paris is amazing city",
+        Weather: "90C"
+    },
+
+
+    {
+        cityName:"Paris",
+        image: "image.png",
+        info: "Paris is amazing city",
+        Weather: "90C"
+    },
+
+
+
+
+    {
+        cityName:"Paris",
+        image: "image.png",
+        info: "Paris is amazing city",
+        Weather: "90C"
+    },
+
+
+
+
+    {
+        cityName:"Paris",
+        image: "image.png",
+        info: "Paris is amazing city",
+        Weather: "90C"
+    },
+]
