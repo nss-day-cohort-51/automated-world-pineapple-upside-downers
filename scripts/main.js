16e2d8b4859e22d1f8e02d6a6ee261c13f78e839
Colin Miller <colinraymiller@gmail.com>
2021-08-24 17:27:31 -0500

    html update

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
 // main.js
+
+import "./Indonesia/main.js";
+// import "Ireland/main.js";
+// import "Greson/main.js";
+// import "Tessema/main.js";
+
+// const landmarkCollection = returnLandmarkCollection();
