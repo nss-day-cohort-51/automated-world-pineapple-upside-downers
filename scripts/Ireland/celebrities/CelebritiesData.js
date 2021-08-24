c9e2106352c5b2bac7821f841e861f09d32ea66c
Colby Ryan <colbyr.ryan@gmail.com>
2021-08-24 11:33:51 -0500

    initial commit

---
+++ scripts/Ireland/celebrities/CelebritiesData.js
@@ -0,0 +1,23 @@
+celebrityCollection = [
+    {
+        name: "blank",
+        population: undefined,
+        description: "Description here",
+        externalLink: "example.html",
+        image: "example.jpg",
+    },
+    {
+        name: "blank",
+        population: undefined,
+        description: "Description here",
+        externalLink: "example.html",
+        image: "example.jpg",
+    },
+    {
+        name: "blank",
+        population: undefined,
+        description: "Description here",
+        externalLink: "example.html",
+        image: "example.jpg",
+    },
+];
---
+++ scripts/Ireland/landmarks/LandmarksData.js
@@ -0,0 +1,23 @@
+const landmarkCollection = [
+    {
+        name: "Old Head of Kinsale",
+        yearCreated: "Instituted 3rd century AD",
+        description: "The Old Head Castle is made up of a long wall with several towers along it, which closes off the headland of the Old Head from the rest of the mainland. ",
+        externalLink: "https://www.oldhead.com/",
+        image: "oldhead.jpg",
+    },
+    {
+        name: "Tralee Golf Club",
+        yearCreated: "Instituted October 1, 1896",
+        description: "Tralee Golf Club opened its present Arnold Palmer-designed course for play at Barrow in October, 1984, after moving from its nine-hole course at Mounthawk near Tralee. It was a great achievement for the Club. Having decided to buy land at Barrow back in 1980, the members dream of seeing the Club attain world-wide status has been realized.",
+        externalLink: "https://traleegolfclub.com/",
+        image: "tralee.jpg",
+    },
+    {
+        name: "Royal Portrush",
+        yearCreated: "Instituted in 1895",
+        description: "Portrush was once a small fishing village which later became a busy seaside holiday resort before Royal Portrush Golf Club emerged as an international golf destination.",
+        externalLink: "https://www.royalportrushgolfclub.com/",
+        image: "royal-portrush.jpg",
+    }
+];
