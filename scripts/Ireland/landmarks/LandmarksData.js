f34673159b6c664cd322e8d51a8f24ffa194e6db
Colin Miller <colinraymiller@gmail.com>
2021-08-31 20:44:15 -0500

    links-again

--- scripts/Ireland/landmarks/LandmarksData.js
+++ scripts/Ireland/landmarks/LandmarksData.js
@@ -10,20 +10,20 @@ const landmarkCollection = [
         yearCreated: 1600,
         description: "The Old Head Castle is made up of a long wall with several towers along it. ",
         externalLink: "https://www.oldhead.com/",
-        image: " /old_head.jpg",
+        image:  "old_head.jpg",
     },
     {
         name: "Tralee Golf Club",
         yearCreated: 1896,
         description: "Tralee Golf Club opened its present Arnold Palmer-designed course for play at Barrow in October, 1984.",
         externalLink: "https://traleegolfclub.com/",
-        image: " /tralee.jpg",
+        image:  "tralee.jpg",
     },
     {
         name: "Royal Portrush",
         yearCreated: 1895,
         description: "Portrush was once a small fishing village which later became a busy seaside holiday resort before Royal Portrush Golf Club emerged.",
         externalLink: "https://www.royalportrushgolfclub.com/",
-        image: " /royal-portrush.jpg",
+        image:  "royal-portrush.jpg",
     }
 ];
